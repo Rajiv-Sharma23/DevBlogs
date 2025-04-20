059bd726e23442120498272988ba34ad259dd03f
Rajiv-Sharma23 <rajivsharma0246@gmail.com>
2025-04-20 09:37:45 +0530

    layout file added clerk added

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,6 +1,13 @@
 import { useState } from "react";
 import { Menu } from "lucide-react";
 import { X } from "lucide-react";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
@@ -9,46 +16,59 @@ const Navbar = () => {
     setOpen(!open);
   };
   return (
-    <div className="w-full flex justify-between items-center h-16 md:h-20 ">
-      {/* Logo */}
-      <div className="flex items-center gap-4 text-2xl font-bold">
-        <img src="/logo.png" alt="logo" className="w-8 h-8" />
-        <span>DevBlogs.</span>
-      </div>
-      {/* Mobile Menu  */}
-      <div className="md:hidden">
-        <div className="cursor-pointer text-4xl " onClick={clickHandler}>
-          {open ? <X /> : <Menu />}
-        </div>
-        {/* menu list */}
-        <div
-          className={`w-full h-screen flex flex-col items-center justify-center 
+    <>
+      <div className="w-full flex justify-between items-center h-16 md:h-20 ">
+        {/* Logo */}
+        <Link to={"/"} className="flex items-center gap-4 text-2xl font-bold">
+          <img src="/logo.png" alt="logo" className="w-8 h-8" />
+          <span>DevBlogs.</span>
+        </Link>
+
+        {/* Mobile Menu  */}
+        <div className="md:hidden">
+          <div className="cursor-pointer text-4xl " onClick={clickHandler}>
+            {open ? <X /> : <Menu />}
+          </div>
+          {/* menu list */}
+          <div
+            className={`w-full h-screen flex flex-col items-center justify-center 
           absolute top-16  ${open ? " right-0 " : "  -right-[100%]"}
-          text-black transition-all duration-500 gap-6 text-2xl font-semibold ` } >
-          <a href="/">Home</a>
-          <a href="/">Trending</a>
-          <a href="/">Most Popular</a>
-          <a href="/">About</a>
-          <a href="/">
+          text-black transition-all duration-500 gap-6 text-2xl font-semibold `}
+          >
+            <Link to={"/"}>Home</Link>
+            <Link to={""}>Trending</Link>
+            <Link to={""}>Most Popular</Link>
+            <Link to={""}>About</Link>
+            <Link to={""}>
+              <button className="py-2 px-4 bg-blue-800 text-white rounded-3xl">
+                Login🖐
+              </button>
+            </Link>
+          </div>
+        </div>
+        {/* Desktop Menu  */}
+        <div className="hidden md:flex items-center gap-8 xl:gap-12 text-lg font-semibold">
+          <Link to={"/"}>Home</Link>
+          <Link to={""}>Trending</Link>
+          <Link to={""}>Most Popular</Link>
+          <Link to={""}>About</Link>
+          <Link to={""}>
             <button className="py-2 px-4 bg-blue-800 text-white rounded-3xl">
               Login🖐
             </button>
-          </a>
+          </Link>
+
+          <div className="flex items-center gap-4 si">
+            <SignedOut>
+              <SignInButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </div>
         </div>
       </div>
-      {/* Desktop Menu  */}
-      <div className="hidden md:flex items-center gap-8 xl:gap-12 text-lg font-semibold">
-        <a href="/">Home</a>
-        <a href="/">Trending</a>
-        <a href="/">Most Popular</a>
-        <a href="/">About</a>
-        <a href="/">
-          <button className="py-2 px-4 bg-blue-800 text-white rounded-3xl">
-            Login🖐
-          </button>
-        </a>
-      </div>
-    </div>
+    </>
   );
 };
 
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,46 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { ClerkProvider } from "@clerk/clerk-react";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import HomePage from "./routes/HomePage.jsx";
+import PostListPage from "./routes/PostListPage.jsx";
+import SinglePostPage from "./routes/SinglePostPage.jsx";
+import Write from "./routes/Write.jsx";
+import LoginPage from "./routes/LoginPage.jsx";
+import RegisterPage from "./routes/Register.jsx";
+import MainLayout from "./layouts/MainLayout.jsx";
 
-createRoot(document.getElementById('root')).render(
+// Import your Publishable Key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
+}
+
+const router = createBrowserRouter([
+  {
+    element: <MainLayout />,
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "/post-list", element: <PostListPage /> },
+      { path: "/:slug", element: <SinglePostPage /> },
+      { path: "/write", element: <Write /> },
+      { path: "/login", element: <LoginPage /> },
+      { path: "/register", element: <RegisterPage /> },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <RouterProvider router={router} />
+    </ClerkProvider>
+  </StrictMode>
+);
