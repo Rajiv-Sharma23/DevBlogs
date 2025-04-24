11f409e9423c8018f6f85768180e4c977a0edfd4
Rajiv-Sharma23 <rajivsharma0246@gmail.com>
2025-04-24 23:29:06 +0530

    optimistic comment added

---
+++ Backend/controllers/comment.controller.js
@@ -0,0 +1,67 @@
+import User from "../models/user.model.js";
+import Comment from "../models/comment.model.js";
+
+export const getPostComments = async (req, res) => {
+  try {
+    const comments = await Comment.find({ post: req.params.postId })
+      .populate("user", "username img")
+      .sort({ createdAt: -1 });
+    res.json({ comments });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error", error });
+    console.log("Error in getPostController", error);
+  }
+};
+export const addComment = async (req, res) => {
+  try {
+    const clerkUserId = req.auth.userId;
+    const postId = req.params.postId;
+
+    if (!clerkUserId) {
+      return res.status(401).json("Not authenticated!");
+    }
+
+    const user = await User.findOne({ clerkUserId });
+
+    const newComment = new Comment({
+      ...req.body,
+      user: user._id,
+      post: postId,
+    });
+
+    const savedComment = await newComment.save();
+
+    setTimeout(() => {
+      res.status(201).json(savedComment);
+    }, 3000);
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error", error });
+    console.log("Error in addComment", error);
+  }
+};
+export const deleteComment = async (req, res) => {
+  try {
+    const clerkUserId = req.auth.userId;
+    const id = req.params.id;
+
+    if (!clerkUserId) {
+      return res.status(401).json("Not authenticated");
+    }
+
+    const user = await User.findOne({ clerkUserId });
+
+    const deletedComment = await Comment.findOneAndDelete({
+      _id: id,
+      user: user._id,
+    });
+
+    if (!deletedComment) {
+      return res.status(403).json("You can delete only your Comments");
+    }
+
+    res.status(200).json("Comment deleted Successfully");
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error", error });
+    console.log("Error in deleteComment", error);
+  }
+};
--- Frontend/src/components/Comments.jsx
+++ Frontend/src/components/Comments.jsx
@@ -1,41 +1,98 @@
 import axios from "axios";
 import Comment from "../components/Comment";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useAuth, useUser } from "@clerk/clerk-react";
+import { toast } from "react-toastify";
 
 const fetchComments = async (postId) => {
   const res = await axios.get(
     `${import.meta.env.VITE_API_URL}/comments/${postId}`
   );
-  return res.data.post;
+  return res.data.comments;
 };
 
 const Comments = ({ postId }) => {
+  const { getToken } = useAuth();
+  const {user} = useUser();
+
   const { isPending, error, data } = useQuery({
     queryKey: ["comments", postId],
     queryFn: () => fetchComments(postId),
   });
 
-  if (isPending) return "Loading...";
+  const queryClient = useQueryClient();
 
-  if (error) return "Something went wrong" + error.message;
+  const mutation = useMutation({
+    mutationFn: async (newComment) => {
+      const token = await getToken(); // make sure getToken is async
+      return axios.post(
+        `${import.meta.env.VITE_API_URL}/comments/${postId}`,
+        newComment,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["comments", postId] });
+    },
+    onError: (error) => {
+      toast.error(error?.response?.data || "Something went wrong");
+    },
+  });
 
-  if (!data) return "Comment not found";
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const data = {
+      desc: formData.get("desc"),
+    };
+    mutation.mutate(data);
+  };
 
   return (
     <div className="flex flex-col gap-8  lg:w-3/5 mb-10">
       <h1 className="text-xl text-gray-500 underline">Comments</h1>
-      <div className="flex items-center justify-between gap-4 w-full">
+      <form
+        onSubmit={handleSubmit}
+        className="flex items-center justify-between gap-4 w-full"
+      >
         <textarea
+          name="desc"
           placeholder="Add your Comments"
           className="flex bg-white rounded-xl p-4 w-full"
         />
         <button className="bg-blue-800 text-white px-4 py-3 font-medium rounded-xl">
           Send
         </button>
-      </div>
-      {data.map((comment) => (
-        <Comment key={comment._id} comment={comment}/>
-      ))}
+      </form>
+      {isPending
+        ? "Loading..."
+        : error
+        ? "error Loading Comments"
+        : 
+        <>
+        {
+          mutation.isPending && (
+            <Comment comment= {{
+              desc : `$mutation.variables.desc (Sending...)`,
+              createdAt: new Date(),
+              user: {
+                img: user.imageUrl,
+                name: user.username,
+              }
+            }} />
+          )
+        }
+
+
+        {data.map((comment) => (
+            <Comment key={comment._id} comment={comment} />
+          ))}
+        </>
+        }
     </div>
   );
 };
